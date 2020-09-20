70a53c3673008d390de57bf871b10760f38c9ad9
Anwesh43 <anweshthecool0@gmail.com>
2020-09-20 19:26:27 +0530

    adding SquareBridge class

--- index.ts
+++ index.ts
@@ -181,3 +181,26 @@ class SBNode {
         return this 
     }
 }
+
+class SquareBridge {
+
+    curr : SBNode = new SBNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
